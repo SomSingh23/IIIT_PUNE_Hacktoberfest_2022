4f4b806101e26a797a839f826a2b16ce542faa32
som <githubsom@gmail.com>
2022-10-19 12:06:38 +0530

    java hw

--- helloWorld.java
+++ helloWorld.java
@@ -1,7 +1,5 @@
-// Code by Shashank Garg ;)
-
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+public class HelloWorld{
+  public static void main(String[] args){
+    System.out.prntln("Hello World");
+  }
 }
