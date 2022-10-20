a54b8233e76a7b0e141ed0e08b5b5f9ed14a793e
Devendra Kumar <115936857+DKChaudhary01@users.noreply.github.com>
2022-10-20 19:46:50 +0530

    Create Devendra_HelloWorld.cpp

---
+++ Devendra_HelloWorld.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+  
+// Main() function: where the execution of program begins
+int main()
+{
+    // prints hello world
+    cout << "Hello World";
+  
+    return 0;
+}
