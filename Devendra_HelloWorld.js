159f0ac4cb3fcf68cc73db9a39d5b8a1eb1afadd
Devendra Kumar <115936857+DKChaudhary01@users.noreply.github.com>
2022-10-20 19:55:11 +0530

    Create Devendra_HelloWorld.js

---
+++ Devendra_HelloWorld.js
@@ -0,0 +1,12 @@
+// Hello_World Program by Devendra
+
+// using alert()
+alert("Hello, World!");
+
+// using console.log
+console.log('Hello World');
+
+// using document.write()
+document.write('Hello, World!');
+
+// Thank You ...
