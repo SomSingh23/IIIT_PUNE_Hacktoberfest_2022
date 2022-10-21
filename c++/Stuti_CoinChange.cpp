71f44084c9b20b94d2f9fd82b73645ebcb37088c
Stuti Verma <70504325+stuti-v4@users.noreply.github.com>
2022-10-21 12:50:40 +0530

    Create Stuti_CoinChange.cpp

---
+++ c++/Stuti_CoinChange.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int count(int coins[], int n, int sum)
+{
+    int i, j, x, y;
+ 
+    // We need sum+1 rows as the table
+    // is constructed in bottom up
+    // manner using the base case 0
+    // value case (sum = 0)
+    int table[sum + 1][n];
+ 
+    // Fill the entries for 0
+    // value case (sum = 0)
+    for (i = 0; i < n; i++)
+        table[0][i] = 1;
+ 
+    // Fill rest of the table entries
+    // in bottom up manner
+    for (i = 1; i < sum + 1; i++) {
+        for (j = 0; j < n; j++) {
+            // Count of solutions including coins[j]
+            x = (i - coins[j] >= 0) ? table[i - coins[j]][j]
+                                    : 0;
+ 
+            // Count of solutions excluding coins[j]
+            y = (j >= 1) ? table[i][j - 1] : 0;
+ 
+            // total count
+            table[i][j] = x + y;
+        }
+    }
+    return table[sum][n - 1];
+}
+ 
+// Driver Code
+int main()
+{
+    int n;
+    cin>>n;
+    int coins[n];
+    for(int i=0;i<n;i++)
+    {
+      cin>>coins[i];
+    }
+    int sum;
+    cin>>sum;
+    cout << count(coins, n, sum);
+    return 0;
+}
